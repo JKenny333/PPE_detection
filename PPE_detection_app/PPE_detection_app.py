60fb5af8f7361cee81670fa45a802a2b1c04ae93
JKenny333 <155671756+JKenny333@users.noreply.github.com>
2024-03-07 10:48:04 +0100

    update

--- PPE_detection_app/PPE_detection_app.py
+++ PPE_detection_app/PPE_detection_app.py
@@ -12,7 +12,7 @@ import time
 
 #=====================================================================================================================
 #File paths
-logo_path = '/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/logos/EagleEye_logo.png'
+logo_path = '/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/logos/EagleEye_logo_green_black_text_no_background.png'
 model_path_s = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/small_yolo_model/ppe_s.pt"
 model_path_m = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/medium_yolo_model/ppe_m.pt"
 model_path_l = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/large_yolo_model/ppe_l.pt"
@@ -23,6 +23,26 @@ model_path_l = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models
 if 'init' not in st.session_state:
     st.session_state['init'] = True
     st.session_state['cap'] = None  # Initial state of the camera is not opened
+    st.session_state['class_info'] = {
+        0: {'class_name': 'Hardhat', 'color': (136, 242, 7), 'include': True, 'positive_class': True, 'type': 'ppe'},
+        1: {'class_name': 'Mask', 'color': (136, 242, 7), 'include': True, 'positive_class': True, 'type': 'ppe'},
+        2: {'class_name': 'NO-Hardhat', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
+            'type': 'violation'},
+        3: {'class_name': 'NO-Mask', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
+            'type': 'violation'},
+        4: {'class_name': 'NO-Safety Vest', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
+            'type': 'violation'},
+        5: {'class_name': 'Person', 'color': (255, 238, 82), 'include': True, 'positive_class': True,
+            'type': 'object'},
+        6: {'class_name': 'Safety Cone', 'color': (255, 255, 0), 'include': True, 'positive_class': True,
+            'type': 'object'},
+        7: {'class_name': 'Safety Vest', 'color': (136, 242, 7), 'include': True, 'positive_class': True,
+            'type': 'ppe'},
+        8: {'class_name': 'machinery', 'color': (255, 105, 180), 'include': True, 'positive_class': True,
+            'type': 'object'},
+        9: {'class_name': 'vehicle', 'color': (255, 105, 180), 'include': True, 'positive_class': True,
+            'type': 'object'}
+    }
 
 if 'model' not in st.session_state:  # Check if the model is already loaded
     st.session_state['model'] = YOLO(model_path_s)  # Load the model and store it in session state
@@ -108,24 +128,27 @@ def update_historical_compliance(current_compliance):
         st.session_state['historical_compliance'].pop(0)
 
 
-#======================================================================================================================
+
 
 
 #======================================================================================================================
 # Main page UI
 
-#Logo
-col1, col2, col3 = st.columns([1,1,1])
-with col2:
-    st.image(logo_path, width=200)  # Adjust width as needed
 
-st.header("PPE Compliance Monitoring System")
+
+#st.header("PPE Compliance Monitoring System")
 
 #Camera feed
 frame_placeholder = st.empty()
 
 #======================================================================================================================
 #Sidebar UI
+
+#Logo
+col1, col2, col3 = st.sidebar.columns([1,2,1])
+with col2:
+    st.image(logo_path, width=125)  # Adjust width as needed
+
 #title
 st.sidebar.markdown("<h1 style='text-align: center; color: #355E66; font-size: 35px; font-weight: bold;'>Control Panel</h1>", unsafe_allow_html=True)
 
@@ -184,6 +207,8 @@ for class_id, info in st.session_state['class_info'].items():
             st.session_state['class_info'][4]['include'] = include
 #======================================================================================================================
 #Analytics
+
+
 st.metric(label="System Frame Rate", value=f"{st.session_state['frame_rate']} fps")
 
 st.metric(label="Current PPE Compliance", value=f"{st.session_state['current_compliance']:.2f}%")
@@ -225,7 +250,7 @@ if st.session_state.get('camera_started', False):
         if stop_button:
             stop_camera()
             break
-
+print(st.session_state['model'])
 #======================================================================================================================
 
 
