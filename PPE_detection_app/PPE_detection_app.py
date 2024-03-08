a26a299ff5f56077fca33f770c4d42b9c8d8b66f
JKenny333 <155671756+JKenny333@users.noreply.github.com>
2024-03-08 19:45:10 +0100

    update

--- PPE_detection_app/PPE_detection_app.py
+++ PPE_detection_app/PPE_detection_app.py
@@ -1,4 +1,4 @@
-#======================================================================================================================
+# ======================================================================================================================
 
 import streamlit as st
 import cv2
@@ -10,39 +10,28 @@ import math
 import os
 import time
 
-#=====================================================================================================================
-#File paths
-logo_path = '/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/logos/EagleEye_logo_green_black_text_no_background.png'
+# =====================================================================================================================
+# File paths
+logo_path = ('/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/logos'
+             '/EagleEye_logo_green_black_text_no_background.png')
 model_path_s = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/small_yolo_model/ppe_s.pt"
 model_path_m = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/medium_yolo_model/ppe_m.pt"
 model_path_l = "/Users/jameskenny/Documents/GitHub/Ironhack/PPE_detection/models/large_yolo_model/ppe_l.pt"
 
-#=====================================================================================================================
-#Initializing session states
+# =====================================================================================================================
+# Initializing session states
 # Initialize the camera and model paths in session state if not already present
 if 'init' not in st.session_state:
     st.session_state['init'] = True
+
+if 'cap' not in st.session_state:
     st.session_state['cap'] = None  # Initial state of the camera is not opened
-    st.session_state['class_info'] = {
-        0: {'class_name': 'Hardhat', 'color': (136, 242, 7), 'include': True, 'positive_class': True, 'type': 'ppe'},
-        1: {'class_name': 'Mask', 'color': (136, 242, 7), 'include': True, 'positive_class': True, 'type': 'ppe'},
-        2: {'class_name': 'NO-Hardhat', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
-            'type': 'violation'},
-        3: {'class_name': 'NO-Mask', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
-            'type': 'violation'},
-        4: {'class_name': 'NO-Safety Vest', 'color': (70, 7, 242), 'include': True, 'positive_class': False,
-            'type': 'violation'},
-        5: {'class_name': 'Person', 'color': (255, 238, 82), 'include': True, 'positive_class': True,
-            'type': 'object'},
-        6: {'class_name': 'Safety Cone', 'color': (255, 255, 0), 'include': True, 'positive_class': True,
-            'type': 'object'},
-        7: {'class_name': 'Safety Vest', 'color': (136, 242, 7), 'include': True, 'positive_class': True,
-            'type': 'ppe'},
-        8: {'class_name': 'machinery', 'color': (255, 105, 180), 'include': True, 'positive_class': True,
-            'type': 'object'},
-        9: {'class_name': 'vehicle', 'color': (255, 105, 180), 'include': True, 'positive_class': True,
-            'type': 'object'}
-    }
+
+if 'camera_started' not in st.session_state:
+    st.session_state['camera_started'] = None
+
+if 'camera_index' not in st.session_state:
+    st.session_state['camera_index'] = 0
 
 if 'model' not in st.session_state:  # Check if the model is already loaded
     st.session_state['model'] = YOLO(model_path_s)  # Load the model and store it in session state
@@ -76,30 +65,44 @@ if 'historical_compliance' not in st.session_state:
 if 'current_compliance' not in st.session_state:
     st.session_state['current_compliance'] = 0  # Variable to store current frame's compliance
 if 'confidence_threshold' not in st.session_state:
-    st.session_state['confidence_threshold'] = 0.25  # Variable to store current frame's compliance
-#======================================================================================================================
-#Defining functions
+    st.session_state['confidence_threshold'] = 0.55  # Variable to store current frame's compliance
 
-#load model
-#store model output in cache
+
+# ======================================================================================================================
+# Defining functions
+
+# load model
+# store model output in cache
 @st.cache_resource
 def load_model(model_path):
     model = YOLO(model_path)
     return model
 
-# Define a function to start the camera
+
+# Function to start the camera
 def start_camera():
     if st.session_state['cap'] is None:
-        st.session_state['cap'] = cv2.VideoCapture(0)  # Start the camera
+        st.session_state['cap'] = cv2.VideoCapture(st.session_state['camera_index'])  # Start the camera
+        st.session_state['cap'].set(3, 1280)  # Set the width to 1280
+        st.session_state['cap'].set(4, 720)  # Set the height to 720
         st.session_state['camera_started'] = True
 
-# Define a function to stop the camera
+
+# Function to stop the camera
 def stop_camera():
     if st.session_state['cap'] is not None:
         st.session_state['cap'].release()  # Release the camera
         st.session_state['cap'] = None
         st.session_state['camera_started'] = False
-        st.success("Camera stopped.")
+        frame_placeholder.empty()  # Clear the placeholder completely
+
+
+# Function to switch the camera input
+def switch_camera():
+    st.session_state['camera_index'] = 1 - st.session_state.get('camera_index', 0)
+    stop_camera()
+    start_camera()
+
 
 def process_frame(frame, model, confidence_threshold, class_info):
     positive_ppe_count = 0
@@ -118,13 +121,28 @@ def process_frame(frame, model, confidence_threshold, class_info):
                     violation_count += 1
                 class_name = class_info[cls]['class_name']
                 color = class_info[cls]['color']
-                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 1)
-                    vzone.putTextRect(frame, f'{class_name} {conf}', (x1, max(35, y1)), scale=0.85, thickness=1, colorB=color, colorT=(0, 0, 0), colorR=color, offset=2)
+
+                # cvzone bounding box
+                w, h = x2 - x1, y2 - y1  # set up for cvzone bounding boxes
+                # Add a rectangle with styled corners to the image
+                cvzone.cornerRect(
+                    frame,  # The image to draw on
+                    (x1, y1, w, h),  # The position and dimensions of the rectangle (x, y, width, height)
+                    l=30,  # Length of the corner edges
+                    t=5,  # Thickness of the corner edges
+                    rt=0,  # Thickness of the rectangle
+                    colorR=(255, 0, 255),  # Color of the rectangle
+                    colorC=color  # Color of the corner edges
+                )
+                # cv2.rectangle(frame, (x1, y1), (x2, y2), color, 1)
+                cvzone.putTextRect(frame, f'{class_name} {conf}', (x1, max(35, y1)), scale=0.85, thickness=1,
+                                   colorB=color, colorT=(0, 0, 0), colorR=color, offset=2)
     # Calculate compliance for the current frame
     total_detections = positive_ppe_count + violation_count
     compliance = (positive_ppe_count / total_detections) * 100 if total_detections > 0 else 100
     st.session_state['current_compliance'] = compliance
     return frame
+
 
 def update_historical_compliance(current_compliance):
     st.session_state['historical_compliance'].append(current_compliance)
@@ -133,45 +151,46 @@ def update_historical_compliance(current_compliance):
         st.session_state['historical_compliance'].pop(0)
 
 
-
-
-
-#======================================================================================================================
+# ======================================================================================================================
 # Main page UI
 
 
+# st.header("PPE Compliance Monitoring System")
 
-#st.header("PPE Compliance Monitoring System")
-
-#Camera feed
+# Camera feed
 frame_placeholder = st.empty()
 
-#======================================================================================================================
-#Sidebar UI
+# ======================================================================================================================
+# Sidebar UI
 
-#Logo
-col1, col2, col3 = st.sidebar.columns([1,2,1])
+# Logo
+col1, col2, col3 = st.sidebar.columns([1, 2, 1])
 with col2:
     st.image(logo_path, width=125)  # Adjust width as needed
 
-#title
-st.sidebar.markdown("<h1 style='text-align: center; color: #355E66; font-size: 35px; font-weight: bold;'>Control Panel</h1>", unsafe_allow_html=True)
+# title
+st.sidebar.markdown(
+    "<h1 style='text-align: center; color: #355E66; font-size: 35px; font-weight: bold;'>Control Panel</h1>",
+    unsafe_allow_html=True)
 
-#Start/Stop camera buttons
+# Start/Stop camera buttons
 st.sidebar.subheader('Camera Controls')
-col4, col5= st.sidebar.columns(2)
+col4, col5, col6 = st.sidebar.columns([1, 1, 1])
 with col4:
     start_button = st.button('Start Camera', on_click=start_camera)
 with col5:
     stop_button = st.button('Stop Camera', on_click=stop_camera)
+with col6:
+    switch_button = st.button('Switch Camera', on_click=switch_camera)
 
-#User input for selecting model
+# User input for selecting model
 model_options = ['YOLO v8 Small', 'YOLO v8 Medium', 'YOLO v8 Large']
 st.sidebar.subheader('Object detection model')
 st.sidebar.markdown("- **Small:** fast but less accurate\n"
                     "- **Medium:** balances speed and accuracy\n"
                     "- **Large:** accurate but slow\n"
-                    "\n<small>Note: Large model requires high-performance hardware for optimal operation.</small>", unsafe_allow_html=True)
+                    "\n<small>Note: Large model requires high-performance hardware for optimal operation.</small>",
+                    unsafe_allow_html=True)
 selected_model = st.sidebar.selectbox('Select model:', model_options, index=0)
 if selected_model == 'YOLO v8 Large':
     st.session_state['model'] = load_model(model_path_l)
@@ -180,7 +199,10 @@ elif selected_model == 'YOLO v8 Medium':
 else:
     st.session_state['model'] = load_model(model_path_s)
 
-#User input for selecting classes to included
+# conf_options = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+st.session_state['confidence_threshold'] = st.sidebar.slider('Select detection confidence threshold:', 0.25, 0.9, 0.55)
+
+# User input for selecting classes to included
 # Sidebar header for user input
 st.sidebar.subheader('Select objects to detect')
 
@@ -210,19 +232,20 @@ for class_id, info in st.session_state['class_info'].items():
             st.session_state['class_info'][3]['include'] = include
         elif class_id == 7:  # Example for Safety Vest
             st.session_state['class_info'][4]['include'] = include
-#======================================================================================================================
-#Analytics
+
+# st.sidebar.button('Reset to Default', on_click=reset_settings)
+# ======================================================================================================================
+# Analytics
 
 
 st.metric(label="System Frame Rate", value=f"{st.session_state['frame_rate']} fps")
 
 st.metric(label="Current PPE Compliance", value=f"{st.session_state['current_compliance']:.2f}%")
-#st.metric(label="Historical PPE Compliance", value=f"{st.session_state['historical_compliance']:.2f}%")
+# st.metric(label="Historical PPE Compliance", value=f"{st.session_state['historical_compliance']:.2f}%")
 
 st.line_chart(st.session_state['historical_compliance'])
 
-
-#======================================================================================================================
+# ======================================================================================================================
 # Main loop for frame processing
 start_time = time.time()
 frame_count = 0
@@ -235,7 +258,8 @@ if st.session_state.get('camera_started', False):
             break
 
         # Process the frame with PPE detection model
-        processed_frame = process_frame(frame, st.session_state['model'], st.session_state['class_info'])
+        processed_frame = process_frame(frame, st.session_state['model'], st.session_state['confidence_threshold'],
+                                        st.session_state['class_info'])
 
         # Display the processed frame in the Streamlit app
         frame_placeholder.image(processed_frame, channels="BGR")
@@ -255,7 +279,5 @@ if st.session_state.get('camera_started', False):
         if stop_button:
             stop_camera()
             break
-print(st.session_state['model'])
-#======================================================================================================================
 
-
+# ======================================================================================================================
